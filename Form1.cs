2c0cc8ea428fb4fe9d41c2b13c6bccb46e04ddb4
Ahmadi <mohsenaahmadi@gmail.com>
2020-02-08 15:20:15 +0330

    Last Convert To Mql

---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string OutPut = "start transaction;\n";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool readheader = false;
+            string outs = OutPut;
+            string outs2 = OutPut;
+            OpenFileDialog od = new OpenFileDialog();
+            od.Multiselect = false;
+            od.Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml";
+            string path = string.Empty;
+            List<string> st = new List<string>();
+            if (od.ShowDialog() == DialogResult.OK)
+            {
+                path = od.FileName;
+                button2.Enabled = true;
+            }
+            using (StreamReader CSV = new StreamReader(path))
+            {
+                while (!CSV.EndOfStream)
+                {
+                    st.Add(CSV.ReadLine());
+                }
+            }
+            for (int i = 1; i < st.Count; i++)
+            {
+                string[] temp = st[i].Split(',');
+                 for (int k = 0; k < 3; k++)
+                {
+                    if (k == 0)
+                    {
+                        outs += "add bus \"eService Object Generator\" \"" + temp[0] + "\" \"\" vault \"eService Administration\" policy \"eService Object Generator\" \"eService Safety Vault\" \"vault_eServiceAdministration\" \"eService Retry Delay\" 1000 \"eService Retry Count\" 5 \"eService Processing Time Limit\" 60 \"eService Name Prefix\" \"" + temp[5] + "\";\n";
+                    }
+                    else if (k == 1)
+                    {
+                        outs += "add bus \"eService Number Generator\" \"" + temp[0] + "\" \"\" vault \"eService Administration\" policy \"eService Object Generator\" \"eService Next Number\" 000001;\n";
+                    }
+                    else
+                    {
+                        outs += "add connection \"eService Number Generator\" from \"eService Object Generator\" \"" + temp[0] + "\" \"\" to \"eService Number Generator\" \""+temp[0]+"\" \"\";\n";
+                    }
+                }
+                outs2 += "modify bus \"eService Number Generator\" \"" + temp[0] + "\" \"\" \"eService Next Number\" 000001;\n";
+            }
+            #region Last Query
+            /*int Dev = 0, Porto = 0, type = 0, revis = 0, name = 0 ;
+            bool readheader = false;
+            for (int j = 0; j < st.Count; j++)
+            {
+                if (st[j] != "")
+                {
+                   List<string> s = new List<string>();
+                   foreach (string u in st[j].Split(','))
+                       s.Add(u);
+                    if (!readheader)
+                    {
+                        Dev = s.IndexOf("Development Cost");
+                        Porto = s.IndexOf("Portotype Cost");
+                        name = s.IndexOf("Name");
+                        type = s.IndexOf("Type");
+                        revis = s.IndexOf("Revision");
+                        readheader = true;
+
+                    }
+                    else
+                    {
+                        int sum = int.Parse(s[Dev]) + int.Parse(s[Porto]);
+                        outs += format(s[type], s[name], s[revis], sum, true)+"\n";
+                        if (sum>100)
+                            outs += format(s[type], s[name], s[revis], 1, false);
+                        else
+                            outs += format(s[type], s[name], s[revis], 0, false);
+
+                    }
+                    outs += "\n";
+                }
+                else
+                    continue;
+                    }
+                */
+                #endregion
+
+            
+            OutPut = outs + "commit transaction;\n"+"|"+ outs2 + "commit transaction;\n";
+
+        }
+        private string format(string type, string name, string revision, int sum , bool DoE)
+        {
+            string output = "modify bus";
+            output += "\""+type+"\""+" ";
+            output += "\"" +name+ "\""+ " ";
+            output += "\"" + revision + "\"" + " ";
+            if (DoE)
+            {
+                output += "description "+ "\"Total Cost is: \""   +" "+sum.ToString()+";";
+            }
+            else
+            {
+                if (sum == 1)
+                {
+                    output += "\"End Item\"" + " YES" + ";";
+                }
+                else 
+                    output += "\"End Item\"" + " NO" + ";";
+            }
+            return output;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] y = OutPut.Split('|');
+            // Displays a SaveFileDialog so the user can save the Image  
+            // assigned to Button2.  
+            for(int i = 0; i < 2; i ++)
+            { 
+            SaveFileDialog save = new SaveFileDialog();
+            if(i==0)
+                    save.FileName = "create.txt";
+            else
+                    save.FileName = "modify.txt";
+                save.Filter = "Text File | *.txt";
+                // If the file name is not an empty string open it for saving.  
+                if (save.FileName != "")
+                {
+                    // Saves the Image via a FileStream created by the OpenFile method.  
+                    // Saves the Image in the appropriate ImageFormat based upon the  
+                    // File type selected in the dialog box.  
+                    // NOTE that the FilterIndex property is one-based.  
+                    if (save.ShowDialog() == DialogResult.OK)
+
+                    {
+                        StreamWriter writer = new StreamWriter(save.OpenFile());
+                        if (i == 0)
+                            writer.Write(y[0]);
+                        else
+                            writer.Write(y[1]);
+                        writer.Dispose();
+                        writer.Close();
+
+                    }
+                }
+            }
+            button2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+        }
+    }
+}
